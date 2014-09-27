3c7d34d86524c03d69db2be669aeb7f40ce2a169
Adam Wójs <adam@wojs.pl>
2014-09-27 16:43:09 +0200

    Utworzono CoreBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Propel\PropelBundle\PropelBundle()
+            new Propel\PropelBundle\PropelBundle(),
+            new Palettes\CoreBundle\PalettesCoreBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
