01e90115792a25e1f96f426c7458067a58e3cf8b
Adam Wójs <adam@wojs.pl>
2014-09-28 13:09:31 +0200

    Utworzenie modelu użytkownika.

---
+++ src/Palettes/CoreBundle/Model/UserPeer.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Palettes\CoreBundle\Model;
+
+use Palettes\CoreBundle\Model\om\BaseUserPeer;
+
+class UserPeer extends BaseUserPeer
+{
+}
---
+++ src/Palettes/CoreBundle/Model/UserQuery.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Palettes\CoreBundle\Model;
+
+use Palettes\CoreBundle\Model\om\BaseUserQuery;
+
+class UserQuery extends BaseUserQuery
+{
+}
