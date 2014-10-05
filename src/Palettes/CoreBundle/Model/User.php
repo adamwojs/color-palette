acb19addf040bd0e08407bcb84aa74481cd6f807
Adam Wójs <adam@wojs.pl>
2014-10-05 11:54:47 +0200

    Konfiguracja bazodanowego providera użytkowników.

--- src/Palettes/CoreBundle/Model/User.php
+++ src/Palettes/CoreBundle/Model/User.php
@@ -3,7 +3,26 @@
 namespace Palettes\CoreBundle\Model;
 
 use Palettes\CoreBundle\Model\om\BaseUser;
+use Symfony\Component\Security\Core\User\UserInterface;
 
-class User extends BaseUser
+
+class User extends BaseUser implements UserInterface
 {
+    private $plainPassword;
+    
+    public function getPlainPassword() {
+        return $this->plainPassword;
+    }
+    
+    public function setPlainPassword($password) {
+        $this->plainPassword = $password;
+    }
+    
+    public function eraseCredentials() {
+        $this->plainPassword = '';
+    }
+
+    public function getRoles() {
+        return [ $this->getRole() ];
+    }
 }
